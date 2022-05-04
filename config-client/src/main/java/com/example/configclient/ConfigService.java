46cf41090f945a6f3b160b6b82fe609d7437f02f
kherwei <kherwei@hotmail.com>
2022-05-04 21:22:09 +0800

    adsASD

--- config-client/src/main/java/com/example/configclient/ConfigService.java
+++ config-client/src/main/java/com/example/configclient/ConfigService.java
@@ -4,13 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 @RefreshScope
 public class ConfigService {
     @Value("${a.b.c}")
     private String var;
 
+    private String _var;
+
+    @PostConstruct
+    public void refresh() {
+        _var = var;
+    }
+
     public String getConfig() {
-        return var;
+        return _var;
     }
 }
